01940e82cf94c6bc571def0fd446bd9cc84e0628
Sam <samandrewmills@gmail.com>
2018-08-29 16:15:25 +1000

    test

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,17 +1,17 @@
 <template>
    <f7-page theme-dark>
     <AppHeader></AppHeader>
-
+ 
     <f7-block>
       <f7-button raised big fill @click="loginScreenOpened = true">Log In</f7-button>
     </f7-block>
     <f7-block>
-      <f7-button raised big fill @click="loginScreenOpened = true">Sign Up</f7-button>
+      <f7-button raised big fill @click="signupScreenOpened = true">Sign Up</f7-button>
     </f7-block>
-
-    <f7-login-screen class="demo-login-screen" :opened="loginScreenOpened" @loginscreen:closed="loginScreenOpened = false">
+ 
+    <f7-login-screen class="login-screen" :opened="loginScreenOpened" @loginscreen:closed="loginScreenOpened = false">
       <f7-page login-screen>
-        <f7-login-screen-title>Framework7</f7-login-screen-title>
+        <f7-login-screen-title>DartParse</f7-login-screen-title>
         <f7-list form>
           <f7-list-item>
             <f7-label>Username</f7-label>
@@ -24,7 +24,28 @@
         </f7-list>
         <f7-list>
           <f7-list-button @click="signIn">Sign In</f7-list-button>
-          <f7-block-footer>Some text about login information.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</f7-block-footer>
+          <f7-block-footer>Welcome to DartParse</f7-block-footer>
+        </f7-list>
+      </f7-page>
+    </f7-login-screen>
+    <f7-login-screen class="signup-screen" :opened="signupScreenOpened" @loginscreen:closed="signupScreenOpened = false">
+      <f7-page login-screen>
+        <f7-login-screen-title>DartParse</f7-login-screen-title>
+        <f7-list form>
+          <f7-list-item>
+            <f7-label>Email</f7-label>
+            <f7-input type="text" placeholder="Your email" @input="email = $event.target.value"></f7-input>
+            <f7-label>Username</f7-label>
+            <f7-input type="text" placeholder="Your username" @input="username = $event.target.value"></f7-input>
+            <f7-label>Password</f7-label>
+            <f7-input type="password" placeholder="Your password" @input="password = $event.target.value"></f7-input>
+            <f7-label>Password (Confirm)</f7-label>
+            <f7-input type="password" placeholder="Your password" @input="password_confirm = $event.target.value"></f7-input>
+          </f7-list-item>
+        </f7-list>
+        <f7-list>
+          <f7-list-button @click="signUp">Sign Up</f7-list-button>
+          <f7-block-footer>Welcome to DartParse</f7-block-footer>
         </f7-list>
       </f7-page>
     </f7-login-screen>
@@ -38,10 +59,11 @@ export default {
     AppHeader
   },
   data() {
-    
-    
+   
+   
     return {
       loginScreenOpened: false,
+      signupScreenOpened: false,
      
     };
   },
@@ -49,8 +71,16 @@ export default {
       signIn() {
         const self = this;
         const app = self.$f7;
-
+ 
         app.dialog.alert(`Username: ${self.username}<br>Password: ${self.password}`, () => {
+          app.loginScreen.close();
+        });
+      },
+      signUp() {
+        const self = this;
+        const app = self.$f7;
+ 
+        app.dialog.alert(`Email: ${self.email}<br>Username: ${self.username}<br>Password: ${self.password}`, () => {
           app.loginScreen.close();
         });
       },
